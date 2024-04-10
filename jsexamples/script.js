7292dc43c1b47157e0fa4351d93aa24e62768ddc
Rohit Ashok Khot <rohitashokkhot@gmail.com>
2024-04-10 23:10:56 +1000

    minor edits

--- jsexamples/script.js
+++ jsexamples/script.js
@@ -2,7 +2,7 @@ function getValue() {
   const input = document.querySelector("#myanswer");
   const inputValue = input.value;
   console.log(inputValue);
-  checkGrade(inputValue);
+  checkGradeStylised(inputValue);
   input.value = "";
 }
 
@@ -17,5 +17,19 @@ function checkGrade(score) {
     answerBox.textContent = "You scored passing marks!!!";
   } else {
     answerBox.textContent = "Sorry, you failed!!!";
+  }
+}
+
+function checkGradeStylised(score) {
+  let answerBox = document.querySelector("#answer");
+  answerBox.innerHTML = "";
+  if (score > 20) {
+    answerBox.innerHTML = `You scored <span class="grade">High Distinction </span>!!!`;
+  } else if (score > 10 && score <= 20) {
+    answerBox.innerHTML = `You scored <span class="grade">Distinction </span>!!!`;
+  } else if (score > 5 && score <= 10) {
+    answerBox.innerHTML = `You scored <span class="grade">Passing marks </span>!!!`;
+  } else {
+    answerBox.innerHTML = `Sorry, you <span class="fgrade">failed </span>!!!`;
   }
 }
