54dbbed481c1a0688938cec11711d211520cd3c3
Rohit Ashok Khot <rohitashokkhot@gmail.com>
2024-05-05 08:51:20 +1000

    added time and duration

--- videoplayer/script.js
+++ videoplayer/script.js
@@ -1,9 +1,18 @@
-const videoList = ["zenscape.mp4", "stardust.mp4"];
+// const videoList = ["zenscape.mp4", "stardust.mp4"];
+const videoList = [
+  { name: "Zenscape", link: "zenscape.mp4" },
+  { name: "Stardust", link: "stardust.mp4" },
+];
 const playPauseButton = document.querySelector("#play-pause-btn");
 playPauseButton.addEventListener("click", togglePlay);
 const playPauseImg = document.querySelector("#play-pause-img");
 const myVideo = document.querySelector("#my-video");
-
+const videoName = document.querySelector("#video-name");
+const videoTime = document.querySelector("#video-time");
+const progressBar = document.querySelector("#progress-bar-fill");
+myVideo.removeAttribute("controls");
+// playPauseBtn.addEventListener("click", togglePlayPause);
+myVideo.addEventListener("timeupdate", updateProgressBar);
 const firstVideoButton = document.querySelector("#first-video-btn");
 firstVideoButton.addEventListener("click", function playIt() {
   myVideo.pause();
@@ -27,7 +36,37 @@ function togglePlay() {
 }
 
 function playVideo(no) {
-  myVideo.src = videoList[no];
+  myVideo.src = videoList[no].link;
+  videoName.textContent = videoList[no].name;
   // myVideo.load();
   // myVideo.play();
 }
+
+function updateProgressBar() {
+  videoTime.textContent = myVideo.currentTime.toFixed(2);
+  const value = (myVideo.currentTime / myVideo.duration) * 100;
+  progressBar.style.width = value + "%";
+}
+
+// Function to toggle fullscreen mode
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    // If no element is in fullscreen, request fullscreen on the video player
+    myVideo.requestFullscreen();
+  } else {
+    // Otherwise, exit fullscreen
+    document.exitFullscreen();
+  }
+}
+
+// Event listener for double-click on the video to toggle fullscreen
+myVideo.addEventListener("dblclick", toggleFullscreen);
+
+// Event listener for fullscreen change event to update UI
+document.addEventListener("fullscreenchange", function () {
+  if (document.fullscreenElement === myVideo) {
+    console.log("Entered fullscreen");
+  } else {
+    console.log("Exited fullscreen");
+  }
+});
