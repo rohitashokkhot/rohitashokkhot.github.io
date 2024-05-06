894db74a422224b3688510d461371e8c44add571
Rohit Ashok Khot <rohitashokkhot@gmail.com>
2024-05-06 20:35:28 +1000

    added comments

--- popp/script.js
+++ popp/script.js
@@ -1,10 +1,13 @@
+// In order to play audio sound on clicking of a button,
+// first we must select both button and audio from the DOM.
 const clickButton = document.querySelector("#clickButton");
 const clickSound = document.querySelector("#clickSound");
 
-// Add event listener to the button for click events
+// We added a click event on the button and added a callback to play sound
 clickButton.addEventListener("click", playSound);
 
 // Function to play the click sound
 function playSound() {
+  // we use the play method to play the sound
   clickSound.play();
 }
