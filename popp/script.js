6f48547789934e1590c67587b5861b1ece265f0b
Rohit Ashok Khot <rohitashokkhot@gmail.com>
2024-05-06 10:45:37 +1000

    update popit

--- popp/script.js
+++ popp/script.js
@@ -1,12 +1,10 @@
-//document.addEventListener("DOMContentLoaded", function () {
-const clickButton = document.getElementById("clickButton");
-const clickSound = document.getElementById("clickSound");
+const clickButton = document.querySelector("#clickButton");
+const clickSound = document.querySelector("#clickSound");
+
+// Add event listener to the button for click events
+clickButton.addEventListener("click", playSound);
 
 // Function to play the click sound
 function playSound() {
   clickSound.play();
 }
-
-// Add event listener to the button for click events
-clickButton.addEventListener("click", playSound);
-//});
