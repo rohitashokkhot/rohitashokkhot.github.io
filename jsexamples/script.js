458dbd5818d8272c40b103cc04d6fce319f29ff3
Rohit Ashok Khot <1760494+rohitashokkhot@users.noreply.github.com>
2024-09-09 11:16:01 +1000

    update js example

--- jsexamples/script.js
+++ jsexamples/script.js
@@ -1,35 +1,32 @@
-function getValue() {
-  const input = document.querySelector("#myanswer");
-  const inputValue = input.value;
-  console.log(inputValue);
-  checkGradeStylised(inputValue);
-  input.value = "";
+function checkGrade() {
+  const a1 = document.querySelector("#answer1");
+  const a1Value = parseFloat(a1.value); // Convert to number
+  console.log(a1Value);
+
+  const a2 = document.querySelector("#answer2");
+  const a2Value = parseFloat(a2.value); // Convert to number
+  console.log(a2Value);
+
+  const total = calculateTotal(a1Value, a2Value);
+  console.log(total);
+  reportBackGrade(total);
+  // a1.value = "";
 }
 
-function checkGrade(score) {
-  let answerBox = document.querySelector("#answer");
-  answerBox.textContent = "";
-  if (score > 20) {
-    answerBox.textContent = "You scored High Distinction!!!";
-  } else if (score > 10 && score <= 20) {
-    answerBox.textContent = "You scored Distinction!!!";
-  } else if (score > 5 && score <= 10) {
-    answerBox.textContent = "You scored passing marks!!!";
-  } else {
-    answerBox.textContent = "Sorry, you failed!!!";
-  }
+function calculateTotal(a, b) {
+  return a + b;
 }
 
-function checkGradeStylised(score) {
-  let answerBox = document.querySelector("#answer");
-  answerBox.innerHTML = "";
-  if (score > 20) {
-    answerBox.innerHTML = `You scored <span class="grade">High Distinction </span>!!!`;
+function reportBackGrade(score) {
+  let report = document.querySelector("#report");
+  report.innerHTML = "";
+  if (score > 30) {
+    report.innerHTML = `You scored <span class="grade">High Distinction </span>!!!`;
+  } else if (score > 20 && score <= 30) {
+    report.innerHTML = `You scored <span class="grade">Distinction </span>!!!`;
   } else if (score > 10 && score <= 20) {
-    answerBox.innerHTML = `You scored <span class="grade">Distinction </span>!!!`;
-  } else if (score > 5 && score <= 10) {
-    answerBox.innerHTML = `You scored <span class="grade">Passing marks </span>!!!`;
+    report.innerHTML = `You scored <span class="grade">Passing marks </span>!!!`;
   } else {
-    answerBox.innerHTML = `Sorry, you <span class="fgrade">failed </span>!!!`;
+    report.innerHTML = `Sorry, you <span class="fgrade">failed </span>!!!`;
   }
 }
