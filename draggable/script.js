125914f7cf56ff02f755ecfc7390eee41b955b3a
Rohit Ashok Khot <1760494+rohitashokkhot@users.noreply.github.com>
2024-10-06 11:27:30 +1100

    temp changes

--- draggable/script.js
+++ draggable/script.js
@@ -4,13 +4,10 @@ let bgColor = null;
 console.log(target);
 boxes.forEach((colorBlock) => {
   colorBlock.addEventListener("dragstart", (event) => {
-    // draggedColor = event.target.style.backgroundColor;
     // Get the background color of the dragged box
     const computedStyle = window.getComputedStyle(event.target);
     bgColor = computedStyle.backgroundColor;
 
-    // Set the data transfer (use 'text/plain' type and store background color)
-    //event.dataTransfer.setData("text/plain", bgColor);
     console.log(bgColor);
   });
 });
@@ -22,10 +19,6 @@ target.addEventListener("dragover", (event) => {
 
 target.addEventListener("drop", (event) => {
   event.preventDefault();
-
-  // Retrieve the background color from the data transfer
-  //const bgColor = event.dataTransfer.getData("text/plain");
-
   // Set the background color of the body to the dropped color
   target.style.backgroundColor = bgColor;
 });
