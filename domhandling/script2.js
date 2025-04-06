481f12411934b2f883b8ee2f496051c38b9376e9
Rohit Ashok Khot <1760494+rohitashokkhot@users.noreply.github.com>
2025-04-06 23:01:13 +1000

    temp dom add

--- domhandling/script2.js
+++ domhandling/script2.js
@@ -13,3 +13,25 @@ for (let i = 0; i < allParas.length; i++) {
   allParas[i].classList.add("redBorder");
   // allParas[i].style.border = "1px solid red";
 }
+
+const myCat = document.querySelector("#my-cat");
+myCat.addEventListener("mouseenter", addMe);
+myCat.addEventListener("mouseleave", removeMe);
+function addMe() {
+  myCat.classList.add("round");
+}
+function removeMe() {
+  myCat.classList.remove("round");
+}
+
+const toggleButton = document.querySelector("#toggle-button");
+toggleButton.addEventListener("click", toggleMe);
+function toggleMe() {
+  myCat.classList.toggle("round");
+  // header.style.backgroundColor = "beige";
+}
+
+// myCat.classList.add("round");
+let myName = "Rohit";
+const header = document.querySelector("header");
+header.innerHTML += `<h2>I am ${myName} </h2>`;
